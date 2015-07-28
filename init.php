97fc1fd418ab01d7ac154a23f81278d0b81aa8fe
jtsternberg <jtsternberg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-28 21:12:23 +0000

    ## 2.0.9 - 2015-07-28
    ### Enhancements
    
    * Updated/Added many translations. Props [fxbenard](https://github.com/fxbenard), ([#203](https://github.com/WebDevStudios/CMB2/pull/344)) and [Mte90](https://github.com/Mte90) for the Italian translation.
    * Updated `'file_list'` field type to have a more intutive selection in the media library, and updated the 'Use file' text in the button. Props [SteveHoneyNZ](https://github.com/SteveHoneyNZ) ([#357](https://github.com/WebDevStudios/CMB2/pull/357), [#358](https://github.com/WebDevStudios/CMB2/pull/358)).
    * `'closed'` group field option parameter introduced in order to set the groups as collapsed by default. Requested in [#391](https://github.com/WebDevStudios/CMB2/issues/391).
    * Added `"cmb2_{$object_type}_process_fields_{$cmb_id}"` hook for hooking in and modifying the metabox or fields before the fields are processed/sanitized for saving.
    * Added Comment Metabox support. Props [GregLancaster71](https://github.com/GregLancaster71) ([#238](https://github.com/WebDevStudios/CMB2/pull/238), [#244](https://github.com/WebDevStudios/CMB2/pull/244)).
    * New "cmb2_{$field_id}_is_valid_img_ext" filter for determining if a field value has a valid image file-type extension.
    
    ### Bug Fixes
    
    * `'multicheck_inline'`, `'taxonomy_radio_inline'`, and `'taxonomy_multicheck_inline'` field types were not outputting anything since it's value was not being returned. Props [ediamin](https://github.com/ediamin), ([#367](https://github.com/WebDevStudios/CMB2/pull/367), ([#405](https://github.com/WebDevStudios/CMB2/pull/405)).
    * `'hidden'` type fields were not honoring the `'show_on_cb'` callback. Props [JPry](https://github.com/JPry), ([commits](https://github.com/WebDevStudios/CMB2/compare/5a4146eec546089fbe1a1c859d680dfda3a86ee2...1ef5ef1e3b2260ab381090c4abe9dc7234cfa0a6)).
    * Fixed: There was no minified cmb2-front.min.css file.
    * Fallback for fatal error with invalid timezone. Props [ryanduff](https://github.com/ryanduff) ([#385](https://github.com/WebDevStudios/CMB2/pull/385)).
    * Fix issues with deleting a row from repeatable group. Props [yuks](https://github.com/yuks) ([#387](https://github.com/WebDevStudios/CMB2/pull/387)).
    * Ensure value passed to `strtotime` in `make_valid_time_stamp` is cast to a string. Props [vajrasar](https://github.com/vajrasar) ([#389](https://github.com/WebDevStudios/CMB2/pull/389)).
    * Fixed issue with Windows IIS and bundling CMB2 in the theme. Props [DevinWalker](https://github.com/DevinWalker), ([#400](https://github.com/WebDevStudios/CMB2/pull/400), [#401](https://github.com/WebDevStudios/CMB2/pull/401))
    
    git-svn-id: https://plugins.svn.wordpress.org/cmb2/trunk@1208582 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- init.php
+++ init.php
@@ -17,7 +17,7 @@
  *               Bill Erickson (@billerickson / billerickson.net)
  *               Andrew Norcross (@norcross / andrewnorcross.com)
  *
- * Version:      2.0.8
+ * Version:      2.0.9
  *
  * Text Domain:  cmb2
  * Domain Path:  languages
@@ -48,7 +48,7 @@
                   or things might explode!
 *************************************************************************/
 
-if ( ! class_exists( 'CMB2_Bootstrap_208', false ) ) {
+if ( ! class_exists( 'CMB2_Bootstrap_209', false ) ) {
 
 	/**
 	 * Handles checking for and loading the newest version of CMB2
@@ -61,7 +61,7 @@ if ( ! class_exists( 'CMB2_Bootstrap_208', false ) ) {
 	 * @license   GPL-2.0+
 	 * @link      http://webdevstudios.com
 	 */
-	class CMB2_Bootstrap_208 {
+	class CMB2_Bootstrap_209 {
 
 		/**
 		 * Current version number
@@ -77,20 +77,20 @@ if ( ! class_exists( 'CMB2_Bootstrap_208', false ) ) {
 		 * @var   int
 		 * @since 2.0.0
 		 */
-		const PRIORITY = 9991;
+		const PRIORITY = 9990;
 
 		/**
-		 * Single instance of the CMB2_Bootstrap_208 object
+		 * Single instance of the CMB2_Bootstrap_209 object
 		 *
-		 * @var CMB2_Bootstrap_208
+		 * @var CMB2_Bootstrap_209
 		 */
 		public static $single_instance = null;
 
 		/**
-		 * Creates/returns the single instance CMB2_Bootstrap_208 object
+		 * Creates/returns the single instance CMB2_Bootstrap_209 object
 		 *
 		 * @since  2.0.0
-		 * @return CMB2_Bootstrap_208 Single instance object
+		 * @return CMB2_Bootstrap_209 Single instance object
 		 */
 		public static function initiate() {
 			if ( null === self::$single_instance ) {
@@ -160,7 +160,7 @@ if ( ! class_exists( 'CMB2_Bootstrap_208', false ) ) {
 				$loaded = load_muplugin_textdomain( 'cmb2', '/languages/' );
 			}
 			if ( ! $loaded ) {
-				$loaded = load_theme_textdomain( 'cmb2', '/languages/' );
+				$loaded = load_theme_textdomain( 'cmb2', get_stylesheet_directory() . '/languages/' );
 			}
 
 			if ( ! $loaded ) {
@@ -173,6 +173,6 @@ if ( ! class_exists( 'CMB2_Bootstrap_208', false ) ) {
 	}
 
 	// Make it so...
-	CMB2_Bootstrap_208::initiate();
+	CMB2_Bootstrap_209::initiate();
 
 }
